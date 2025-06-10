a45e0e74719876348aa8df5a295164a09578eb5e
gkdms0910 <73287609+gkdms0910@users.noreply.github.com>
2025-06-10 16:08:43 +0900

    디자인 프레임 작성

--- app/src/main/java/com/example/project_t2/screens/MainScreen.kt
+++ app/src/main/java/com/example/project_t2/screens/MainScreen.kt
@@ -2,34 +2,48 @@ package com.example.project_t2.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.project_t2.R
 import com.example.project_t2.graphics.RenderImage
 
 import com.example.project_t2.graphics.cloudImage
 import com.example.project_t2.graphics.sunImage
+import com.example.project_t2.graphics.titleText
 import com.example.project_t2.ui.theme.MainFont
 
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
+fun MainScreen(modifier: Modifier = Modifier, navController: NavController) {
     val cloud = cloudImage()
     val sun = sunImage()
+    val title = titleText()
+
     Box(
         modifier = modifier
             .fillMaxSize()
+            .clickable {
+                navController.navigate("diary")
+            }
     ) {
+        // 배경 이미지
         Image(
             painter = painterResource(id = R.drawable.paper_texture),
             contentDescription = null,
@@ -37,44 +51,37 @@ fun MainScreen(modifier: Modifier = Modifier) {
             modifier = Modifier.fillMaxSize()
         )
 
+        // 콘텐츠
         Box(
             modifier = Modifier
                 .fillMaxSize()
                 .background(
                     Brush.verticalGradient(
                         colors = listOf(
-                            Color(0x807cccf4), // semi-transparent
-                            Color(0x80FFFFFF)
+                            Color(0xD37CCCF4),
+                            Color(0xA9FFFFFF)
                         )
                     )
-                ),
-            //contentAlignment = Alignment.Center
+                )
         ) {
+            // 타이틀 이미지
             RenderImage(
-                sun, modifier = Modifier
+                title, modifier = Modifier
                     .fillMaxWidth(0.8f)
-                    .offset(x = 210.dp, y = -50.dp)
+                    .align(Alignment.TopCenter)
+                    .padding(top = 200.dp)
             )
+            // 구름1
             RenderImage(
                 cloud, modifier = Modifier
                     .fillMaxWidth(0.4f)
                     .offset(x = 10.dp, y = 300.dp)
             )
+            //구름 2
             RenderImage(
                 cloud, modifier = Modifier
                     .fillMaxWidth(0.7f)
-                    .offset(x = 180.dp, y = 600.dp)
-            )
-            Text(
-                text = "대체로 맑음",
-                fontSize = 50.sp,
-                //fontWeight = FontWeight.Bold,
-                fontFamily = MainFont,
-                color = Color(0xFF333333),
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .padding(top = 200.dp)
-                    .align(Alignment.TopCenter)
+                    .offset(x = 180.dp, y = 500.dp)
             )
         }
     }
@@ -83,5 +90,6 @@ fun MainScreen(modifier: Modifier = Modifier) {
 @Preview
 @Composable
 private fun PreviewMainScreen() {
-    MainScreen()
+    MainScreen(modifier=Modifier,navController = rememberNavController())
+
 }
