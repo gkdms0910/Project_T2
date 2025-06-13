ffe45a3145b6b7120b174a3d4ba82e409c454a46
dldyou <chan12_yun@naver.com>
2025-06-13 21:05:59 +0900

    feat: WSentimentViewModel 추가

---
+++ app/src/main/java/com/example/project_t2/viewmodel/WSentimentViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.project_t2.viewmodel
+
+import android.media.CamcorderProfile.getAll
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.project_t2.roomDB.WSentiment.WSentimentEntity
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+
+class WSentimentViewModel(private val repository: WSentimentRepository) : ViewModel() {
+    private var _wsentimentList = MutableStateFlow<List<WSentimentEntity>>(emptyList())
+    val wsentimentList = _wsentimentList
+
+    init {
+        findAll()
+    }
+
+    fun findAll() {
+        viewModelScope.launch {
+            repository.findAll().collect {
+                _wsentimentList.value = it
+            }
+        }
+    }
+
+    fun InsertItem(item: WSentimentEntity) {
+        viewModelScope.launch {
+            repository.insertItem(item)
+        }
+    }
+
+    fun UpdateItem(item: WSentimentEntity) {
+        viewModelScope.launch {
+            repository.updateItem(item)
+        }
+    }
+
+    fun DeleteItem(item: WSentimentEntity) {
+        viewModelScope.launch {
+            repository.deleteItem(item)
+        }
+    }
+}
