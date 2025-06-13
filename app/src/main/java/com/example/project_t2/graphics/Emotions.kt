3b01df296b5aea0481953f4190b25a6023e2cfff
dldyou <chan12_yun@naver.com>
2025-06-13 21:05:36 +0900

    style: TODO

--- app/src/main/java/com/example/project_t2/graphics/Emotions.kt
+++ app/src/main/java/com/example/project_t2/graphics/Emotions.kt
@@ -6,3 +6,5 @@ enum class Emotion(val displayName: String, val emoji: String, val unicode: Stri
     SAD("슬픔", "\uD83D\uDE22", "U+1F622"),    // 😢
     ANGRY("화남", "\uD83D\uDE20", "U+1F620")   // 😠
 }
+
+//TODO: 감정은 Angry, Fear, Happy, Tender, Sad 5가지로 분류하도록 변경해주세요.
