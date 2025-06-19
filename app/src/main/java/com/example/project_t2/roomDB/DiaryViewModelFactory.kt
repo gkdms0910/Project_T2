299c335d7ee05780aa998111bc69d09896664d78
9rydud <9rydud@gmail.com>
2025-06-19 18:02:11 +0900

    Add files via upload

---
+++ app/src/main/java/com/example/project_t2/roomDB/DiaryViewModelFactory.kt
@@ -0,0 +1,14 @@
+package com.example.p2w.roomDB
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class DiaryViewModelFactory(private val repository: DiaryRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(DiaryViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return DiaryViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
