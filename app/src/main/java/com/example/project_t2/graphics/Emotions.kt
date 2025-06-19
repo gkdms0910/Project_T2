5964cf3d9395c5348c263e3e4ca8aa85283921b1
9rydud <9rydud@gmail.com>
2025-06-19 17:57:14 +0900

    Update Emotions.kt

--- app/src/main/java/com/example/project_t2/graphics/Emotions.kt
+++ app/src/main/java/com/example/project_t2/graphics/Emotions.kt
@@ -1,10 +1,9 @@
 package com.example.project_t2.graphics
 
 enum class Emotion(val displayName: String, val emoji: String, val unicode: String) {
-    SMILE("미소", "\uD83D\uDE0A", "U+1F60A"),   // 😊
-    BORED("지루함", "\uD83D\uDE12", "U+1F612"), // 😒
-    SAD("슬픔", "\uD83D\uDE22", "U+1F622"),    // 😢
-    ANGRY("화남", "\uD83D\uDE20", "U+1F620")   // 😠
+    ANGRY("화남", "\uD83D\uDE20", "U+1F620"),     // 😠
+    FEAR("두려움", "\uD83D\uDE31", "U+1F631"),     // 😱
+    HAPPY("행복", "\uD83D\uDE04", "U+1F604"),     // 😄
+    TENDER("평온", "\uD83D\uDE0C", "U+1F60C"),     // 😌
+    SAD("슬픔", "\uD83D\uDE22", "U+1F622")        // 😢
 }
-
-//TODO: 감정은 Angry, Fear, Happy, Tender, Sad 5가지로 분류하도록 변경해주세요.
